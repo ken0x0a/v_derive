218fc6f18aec671fefdea0a033048055f448f19e
Ken <ken0x0a+github@gmail.com>
2022-04-15 10:08:35 +0900

    feat: add `bincode`

--- bin/derive/gen_code.v
+++ bin/derive/gen_code.v
@@ -6,6 +6,9 @@ import macro { Custom, Derive, Macro }
 import derive.json.de as deser_json
 import derive.as_map
 import derive.json.ser as ser_json
+import derive.bincode.ser as ser_bin
+import derive.bincode.de as deser_bin
+import derive.bincode.common as bin_common
 import derive.clone as derive_clone
 import codegen { Codegen }
 
@@ -47,6 +50,14 @@ pub fn gen_code(mut gen Codegen, macro Macro, decl GenCodeDecl) {
 
 pub fn gen_derive_for_enum(mut gen Codegen, macro_name string, decl EnumDecl) {
 	match macro_name {
+		ser_bin.macro_name {
+			bin_common.add_len_fn_for_enum(mut gen, decl)
+			ser_bin.add_encode_fn_for_enum(mut gen, decl)
+		}
+		deser_bin.macro_name {
+			bin_common.add_len_fn_for_enum(mut gen, decl)
+			deser_bin.add_decode_fn_for_enum(mut gen, decl)
+		}
 		ser_json.macro_name {
 			gen.add_import_if_not_exist('x.json2')
 			ser_json.add_encode_json_enum(mut gen, decl)
@@ -63,6 +74,14 @@ pub fn gen_derive_for_enum(mut gen Codegen, macro_name string, decl EnumDecl) {
 
 pub fn gen_derive_for_struct(mut gen Codegen, macro_name string, decl StructDecl) {
 	match macro_name {
+		ser_bin.macro_name {
+			bin_common.add_len_fn_for_struct(mut gen, decl)
+			ser_bin.add_encode_fn_for_struct(mut gen, decl)
+		}
+		deser_bin.macro_name {
+			bin_common.add_len_fn_for_struct(mut gen, decl)
+			deser_bin.add_decode_fn_for_struct(mut gen, decl)
+		}
 		derive_clone.macro_name {
 			derive_clone.add_derive(mut gen, decl)
 		}
